34adcb1727d6bc60dd5331ead67ac8aedcb73b42
Joseph Misiti <joseph.misiti@gmail.com>
2015-07-22 23:51:38 -0400

    moving stuff around

--- README.md
+++ README.md
@@ -22,9 +22,20 @@ Then execute the following command:
 $ cookiecutter https://github.com/josephmisiti/generic-django-project-heroku.git
 ```
 
+
+heroku buildpacks:set git://github.com/heroku/heroku-buildpack-python.git
+
+
 #### Setting Up The Database
 
-TODO
+Login to Heroku and go here:
+
+https://postgres.heroku.com/databases
+
+Choose `Dev Plan` (free) and you should end up with a postgres database
+
+https://postgres.heroku.com/databases/heroku-postgres-da4ace14-heroku-postgresql-olive
+
 
 
 If you have any questions or what to help out, send a pull request or reach out at [@josephmisiti](http://www.twitter.com/josephmisiti)
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/pipeline_configs.py
@@ -0,0 +1,54 @@
+PIPELINE_JS = {
+	'global': {
+		'source_filenames': (	
+		'js/jquery-1.7.min.js',
+        	'js/jquery.progressbar.js',
+        	'js/jquery.metadata.js',
+        	'js/calendar.js',
+        	'js/ajax.js',
+        	'js/jquery.validate.js',
+        	'js/jquery.tooltip.js',
+        	'js/flowplayer.min.js',
+        	'js/jquery-ui-1.8.16.custom.min.js',
+        	'js/underscore-min.js',
+        	'js/backbone-min.js',
+        	'js/sigma.js',
+        	'js/form_validation.js',
+        	'js/training.js',
+        	'js/settings.js',
+        	'js/fileuploader.js',
+        	'js/training-drag-and-drop.js',
+        	'js/user_settings_saved.js',
+        	'js/testing.js',
+        	'js/GeneralAnimations.js',
+        	'js/bootstrap-modal.js',
+        	'js/edit_account_settings.js',
+        	'js/spectrum_training.js',
+        	'js/easySlider1.7.js',
+        	'js/legacy.js',
+        	'js/login.js',
+        	'js/select2.js',
+        	'js/sigma-validation.js',
+		),
+		'output_filename': 'js/bundle-global.js',
+	},	
+}
+
+
+PIPELINE_CSS = {
+	'global': {
+		'source_filenames': (
+			'css/Sigma_MainStyle.css',
+			'css/calendar.css',
+			'css/fileuploader.css',
+			'css/jquery.custom.css',
+			'css/jquery.modal.css',
+			'css/sigma_training_modules.css',
+			'css/user_settings_saved.css',
+			'css/easyslider.css',
+			'css/select2.css',
+			'css/IE.css',
+		),
+		'output_filename': 'css/bundle-global.css',
+	},
+}
