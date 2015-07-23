7ce887761a10a98381c5add6ac8204b1a6765071
Joseph Misiti <joseph.misiti@gmail.com>
2015-07-23 13:49:10 -0400

    updates

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'apps.static.views.index', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
