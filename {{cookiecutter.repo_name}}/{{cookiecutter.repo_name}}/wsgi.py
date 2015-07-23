cb8bba0fea243c2a175b70c914ee1cccf6b2be39
Joseph Misiti <joseph.misiti@gmail.com>
2015-07-23 13:39:35 -0400

    whoops

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/wsgi.py
@@ -12,6 +12,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_name}}.settings")
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
